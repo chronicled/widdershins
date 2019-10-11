417d321146fa5952167779750b992780f4d14a70
Ryan Sundberg <ryansundberg@chronicled.com>
2019-10-11 16:54:26 -0700

    Feature parity with js version

--- src/widdershins/asyncapi1/core.cljs
+++ src/widdershins/asyncapi1/core.cljs
@@ -1,6 +1,42 @@
 (ns widdershins.asyncapi1.core)
 
+(defn- convert-message
+  [message-mode message]
+  (when (some? message)
+    {message-mode message}))
+
+(defn- convert-topic
+  [{:keys [parameters publish subscribe] :as topic}]
+  {:messages (merge (convert-message :publish publish)
+                    (convert-message :subscribe subscribe)) 
+   :parameters parameters})
+
+(defn- convert-topics
+  [{:keys [x-exchanges x-queues topics]}]
+  (->> topics
+       (map (fn [[topic-name topic]]
+              (when topic [topic-name (convert-topic topic)])))
+       (remove nil?)
+       (into {})))
+
+(defn- convert-section
+  [section]
+  (let [section-title (get-in section [:info :title] "Untitled")
+        section-topics (convert-topics section)
+        section-body {:description (get-in section [:info :description])
+                      :externalDocs (get-in section [:info :externalDocs])
+                      :topics section-topics}]
+    [section-title section-body]))
+
+(defn- do-convert-to-toc
+  [sources]
+  (->> sources
+       (map convert-section)
+       (into {})))
+
 (defn ^:export convert-to-toc
   [sources]
-  (println "convert-to-toc")
-  (clj->js {:foo "bar"}))
+  (-> sources
+      (js->clj :keywordize-keys true)
+      (do-convert-to-toc)
+      (clj->js)))
